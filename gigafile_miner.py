6bad45b82c7d14dce702b1f3e8cfb868995e63a2
Hello-Rody <89099513+Hello-Rody@users.noreply.github.com>
2023-03-26 14:48:42 +0900

    Update gigafile_miner.py

--- gigafile_miner.py
+++ gigafile_miner.py
@@ -1,5 +1,5 @@
 import requests
-from random import choice
+import random
 
 lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
          "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -11,7 +11,7 @@ num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 def rand_str(l):
     random_string = ""
     for lengh in range(l):
-        one = choice(lower + upper + num)
+        one = random.choice(lower + upper + num)
         random_string += one
     return random_string
 
@@ -20,12 +20,13 @@ url = "https://xgf.nu/"
 
 try:
     while True:
-        string = rand_str(4)
-        true_url = url + string
-        get = requests.get(true_url)
+        rand_num = random.randint(4, 5)
+        string = rand_str(rand_num)
+        new_url = url + string
+        print("\r[+] checking... [ " + string + " ] ", end=" ")
+        get = requests.get(new_url, timeout=6)
         get_url = get.url
-        print("\r[+]checking... [ " + string + " ]", end="")
         if len(get_url) > 30:
-            print("\r[+]collect!! --> " + get_url)
+            print("\r[+] collect!! --> " + new_url)
 except KeyboardInterrupt:
     print("\nbye")
